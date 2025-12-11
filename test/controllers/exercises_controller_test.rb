8f1a7d472b1fa63669862e5d7dfdb8a9018db115
Ale Bardin <alejandro.bardin01@gmail.com>
2025-12-11 02:17:34 -0300

    fix excercises controller test

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ExercisesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get exercises_show_url
+    get exercise_url(exercises(:one))
     assert_response :success
   end
 end
